71c40008be3ae03e9d4a3690ca88899e43834c25
James <95957855+KILLHU@users.noreply.github.com>
2024-07-20 10:35:49 +0700

    config.js

--- config.js
+++ config.js
@@ -41,9 +41,9 @@ module.exports = {
   nodes: [
     {
         name: "Node #1",
-        host: "lava-v3.ajieblogs.eu.org",
-        port: 80,
-        password: "https://dsc.gg/ajidevserver",
+        host: "fi3.bot-hosting.net",
+        port: 22438,
+        password: "coders",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
